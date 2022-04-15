8e411e9545ccb77edf0691c25af81dac3ed31827
Norem29 <jo.norem29@gmail.com>
2022-04-15 15:27:22 +0200

    Composant Header -> Ajout des props

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,31 @@
 import './header.scss';
 import PropTypes from 'prop-types';
 
-const Header = () => (
+const Header = ({
 
-  <div>Header</div>
+  baseAmount,
+  baseCurrency,
+  className
+}) => (
+
+  <header className={`header ${className}`}>
+    <h1 className='header-title'>Converter</h1>
+    <p className='header-amount'>{baseAmount} {baseCurrency}</p>
+  </header>
 );
 
 Header.propTypes = {
 
+  baseAmount: PropTypes.number,
+  baseCurrency: PropTypes.string,
+  className: PropTypes.string,
 }
 
 Header.defaultProps = {
 
-
+  baseAmount: 1,
+  baseCurrency: 'euro',
+  className: '',
 }
 
 export default Header;
