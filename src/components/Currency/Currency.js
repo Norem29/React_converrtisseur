9d3fdd707713fbf326f5e7fdc7b9f5c21172dce6
Norem29 <jo.norem29@gmail.com>
2022-04-20 17:50:02 +0200

    Style de la liste des currencies

--- src/components/Currency/Currency.js
+++ src/components/Currency/Currency.js
@@ -1,11 +1,11 @@
 import './currency.scss';
 import PropTypes from 'prop-types'
 
-const Currency = ({name}) => {
-  return (
-    <li className='currency'>{name}</li>
-  );
-}
+const Currency = ({name}) => (
+  
+  <li className='currency'>{name}</li>
+);
+
 
 Currency.propTypes = {
 
